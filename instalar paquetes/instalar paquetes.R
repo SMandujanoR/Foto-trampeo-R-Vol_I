ebf2f42f35df74884da560d492e21a8c755430e8
Salvador Mandujano Rodríguez <32318328+SMandujanoR@users.noreply.github.com>
2019-08-11 12:42:44 -0500

    Add files via upload

---
+++ instalar paquetes/instalar paquetes.R
@@ -0,0 +1,27 @@
+# Para instalar los paquetes que se emplean en este libro, se sugiere ejectuar este código:
+  
+paquetes <- c("imager", "vegan", "unmarked", "secr", 
+              "agricolae", "wiqid", "SPACECAP", "jagsUI", "iNEXT", 
+              "BiodiversityR", "overlap", "circular", "camtrapR", 
+              "rgdal", "ggplot2", "PerformanceAnalytics", 
+              "RColorBrewer", "MASS", "MuMin",   "bibtex", "dplyr", 
+              "ade4", "adehabitatHR", "adehabitatLT", "agricolae", 
+              "akima", "boot", "chron", "dunn.test", "ggExtra", 
+              "ggplot2", "gtools", "jagsUI", "KernSmooth", 
+              "maptools", "MASS", "mgcv", "overlap", "plotrix", 
+              "raster", "RColorBrewer", "reshape", "rgdal", 
+              "rgeos", "rmarkdown", "scrbook", "secr", "sp", 
+              "survival", "survminer", "tree", "unmarked", 
+              "vegan", "wiqid", "maps", "GIStools", "WDI", 
+              "Scales", "shapefiles", "RgoogleMaps", "tidyverse",
+              "data.table", "reshape2", "effects")
+  
+pkgs_miss <- paquetes[!(paquetes %in% 
+                        installed.packages())]
+    
+if(length(pkgs_miss)>0L)
+      install.packages(pkgs_miss, repos = 
+      "https://cloud.r-project.org/", 
+                          dependencies = TRUE)
+
+# Alternativamente se pueden instalar uno por uno cada paquete desde RStudio/Packages/Install. Se sugiere instalar simultáneamente todas las dependencias asociadas a cada paquete.
