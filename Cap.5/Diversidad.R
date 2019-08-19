7a762dbccc9adba890da90e7191adb47baf9438c
Salvador Mandujano Rodríguez <32318328+SMandujanoR@users.noreply.github.com>
2019-08-19 16:15:19 -0500

    Add files via upload

--- Cap.5/Diversidad.R
+++ Cap.5/Diversidad.R
@@ -1,33 +1,30 @@
 
-#Cargar la librería
 library(vegan)
-
-#Cargar la matriz de datos 
 
 diversidad <- read.csv("mamiferos.csv", row.names=1) #Las especies deben de estar acomodadas en las columnas y en las filas las cámaras-trampa (sitios)
 View(diversidad)
 
 # Calcular los índices:
 
-N0 <- rowSums(diversidad >0) # Riqueza específica (N0) 
+N0 <- rowSums(diversidad >0) 
 N0
 
-H<-diversity(diversidad, index="shannon") # Índice de Shannon-Wiener
+H<-diversity(diversidad, index="shannon") 
 H
 
-J <- H/log(N0)     # Equidad de Pielou
+J <- H/log(N0)    
 J
 
-simp <- diversity(diversidad, "simpson") #Índice de Simpson
+simp <- diversity(diversidad, "simpson") 
 simp
 
-invsimp <- diversity(diversidad, "inv") # Inverso de Simpson
+invsimp <- diversity(diversidad, "inv") 
 invsimp
 
-N1 <- exp(H)   # Serie de números de Hill
+N1 <- exp(H)  
 N1
 
-# Promedio y desviación estándar de la riqueza específica y la serie de números de Hill para todas las cámaras-trampa
+# Estadísticos
 
 mean(N0)  #Riqueza específica (N0)
 sd(N0)
@@ -35,7 +32,7 @@ sd(N0)
 mean(N1) # Serie de números de Hill
 sd(N1)
 
-#Creamos una tabla que agrupe los resultados:
+#Creamos una tabla que agrupe los resultados (Fig. 5.2):
 diversidad_mamiferos <- round(data.frame(N0, H, J, simp, invsimp, N1),2)
 View(diversidad_mamiferos)
 
